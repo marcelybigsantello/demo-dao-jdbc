e31aa6928a107ff155de03e00b61f53fc21a5d82
Marcely Santello <marcely@sbsoftware.com.br>
2020-12-23 14:27:52 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,9 @@ public class Program {
 		// TODO Auto-generated method stub
 
 		Department d1 = new Department(01, "Books", "Department of generic and specialized books");
-		System.out.println(d1);
+		
+		Seller s1 = new Seller(20, "Bob", "bob@gmail.com.br", new Date(), 2400.00f, d1);
+		System.out.println(s1);
 	}
 
 }
