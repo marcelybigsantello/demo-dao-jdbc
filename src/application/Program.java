ecfd0fecf5fb075a3ea994144f56990c2a510693
Marcely Santello <marcely@sbsoftware.com.br>
2020-12-23 14:17:16 -0300

    Department Entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Department d1 = new Department(01, "Books", "Department of generic and specialized books");
+		System.out.println(d1);
+	}
+
+}
