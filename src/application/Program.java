733fab1c23d517562ab9981b3d67be2364b7c0de
Marcely Santello <marcely@sbsoftware.com.br>
2020-12-23 14:45:54 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +14,8 @@ public class Program {
 		Department d1 = new Department(01, "Books", "Department of generic and specialized books");
 		
 		Seller s1 = new Seller(20, "Bob", "bob@gmail.com.br", new Date(), 2400.00f, d1);
+		
+		SellerDAO sellerDAO = DaoFactory.createSellerDao();
 		System.out.println(s1);
 	}
 
